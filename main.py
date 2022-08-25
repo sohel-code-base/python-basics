7ffd19739578349953dc7b25dbb1931202756486
soheldeveloper <sohelrananayanpur@gmail.com>
2022-08-25 23:54:09 +0600

    input

--- main.py
+++ main.py
@@ -47,3 +47,19 @@ aBc = 5.80
 print(abc, Abc, aBc)
 
 print(len(Abc))
+
+
+# inputs
+#
+# n = input("Enter the name: ")
+# print("Hello", n)
+
+str_seconds = input("Enter the number of seconds: ")
+total_secs = int(str_seconds)
+
+hours = total_secs // 3600
+secs_still_remaining = total_secs % 3600
+minutes = secs_still_remaining // 60
+secs_finally_remaining = secs_still_remaining % 60
+
+print("Hours: ", hours, "mins: ", minutes, "secs: ", secs_finally_remaining)
