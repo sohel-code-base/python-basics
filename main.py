1abdb1b373178163d59b5f32d2e1f3f4b34218bb
soheldeveloper <sohelrananayanpur@gmail.com>
2022-08-25 23:22:41 +0600

    types, string, type casting

--- main.py
+++ main.py
@@ -1,3 +1,24 @@
 print("Hello Python")
 
 print(10//3)
+
+print(2**3)
+
+# types
+print(type("Hello World"))
+print(type(10))
+print(type(10.30))
+
+# String
+
+print("""It's a computer programming language""")
+print("It's a computer programming language")
+print('''It's a computer programming language''')
+
+
+# type converting
+print(3.76, int(3.76))
+print(3, float(3))
+print(-3.98, float(-3.98))
+print("320", int("320"))
+print(int("20pens"))
