5b627621233c7c26245dc9031c7216f0e4670457
soheldeveloper <sohelrananayanpur@gmail.com>
2022-08-27 00:00:05 +0600

    list

---
+++ list.py
@@ -0,0 +1,12 @@
+# fruits = ['Apple', 'Mango', 'Banana']
+fruits = list(('Mango', 'Banana', 'Orange', 'Grape', 'Apple'))
+
+# fruits.pop(0) #remove element
+# fruits.sort()
+# print(fruits.count('Apple'))
+# fruits.reverse()
+# fruits.insert(1, 'Date')
+# fruits.append('Pineapple') #insert in last
+# fruits.extend('Guava')
+for fruit in fruits:
+    print(fruit)
