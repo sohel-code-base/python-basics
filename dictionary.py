687a57ce60b7f597988cf6200e84d6d7cc3a0c15
soheldeveloper <sohelrananayanpur@gmail.com>
2022-08-27 00:11:13 +0600

    dictionary updated

--- dictionary.py
+++ dictionary.py
@@ -4,8 +4,11 @@ profile = {
     'hobbies': ['Reading books', 'Playing Cards', 'Learn technologies']
 }
 
-profile.pop('name')
-
+# profile.pop('name')
+# print(profile.get('age'))
+# print(profile.keys())
+# print(profile.items())
+# profile.popitem()
 try:
     hobbies = profile['hobbies']
 
